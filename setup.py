22c234e23ae02ced374cd38ffb1ce17b24cd6e3c
Ross Jones <ross@mailbolt.com>
2018-05-14 12:03:48 +0100

    Remove the extraneous folder in namespace_packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/datagovuk/ckanext-dgu-local',
     license='GPL3',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext', 'ckanext.dgulocal'],
+    namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
